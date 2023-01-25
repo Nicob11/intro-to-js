c664456f927a0e136d57bfd46e17dbf35e7b08de
NicoB <nicobasma_@hotmail.com>
2023-01-25 19:30:51 +0000

    working in class

--- 04.-strings.js
+++ 04.-strings.js
@@ -11,10 +11,10 @@
 // 1. Introducir saltos de línea sin utilizar el escape \n
 // 2. Introducir variables de JS dentro de la propia string
 
-// const hello = 'Hello';
-// const world = 'World';
-// const fullString = '' // A COMPLETAR POR LOS ESTUDIANTES
-// console.log(fullString);
+// // const hello = 'Hello';
+// // const world = 'World';
+// // const fullString = '' // A COMPLETAR POR LOS ESTUDIANTES
+// // console.log(fullString);
 
 // Podemos concatenar strings con el operador +
 
@@ -24,39 +24,50 @@
 // console.log(fullString);
 
 // Además, las strings tienen una propiedad length...
+ const leaveThisUncommented = '   This is an Example String. Use your methods here   ';
 
 // console.log(leaveThisUncommented.length);
 
 // ...que nos permite utilizarlas como arrays
 
-// console.log(leaveThisUncommented[1]);
+ //console.log(leaveThisUncommented[1]);
 // console.log(leaveThisUncommented[6]);
 // console.log(leaveThisUncommented[0]);
 // console.log(leaveThisUncommented[leaveThisUncommented.length - 1]);
 
 // Por último, podemos utilizar los métodos nativos de JavaScript
 
-// const leaveThisUncommented = '   This is an Example String. Use your methods here   ';
 
 // EJERCICIOS CON MÉTODOS
 
 // 1.- Imprime leaveThisUncommented todo en minúsculas
-
+const tolowercase = leaveThisUncommented.toLowerCase();
+console.log(tolowercase)
 // 2.- Imprime leaveThisUncommented todo en mayúsculas
-
+const touppercase = leaveThisUncommented.toUpperCase();
+console.log(touppercase)
 // 3.- Quita los espacios iniciales y finales de leaveThisUncommented
-
+const withoutspaces = leaveThisUncommented.trim()
+console.log(withoutspaces)
 // 4.- Determina si leaveThisUncommented incluye la palabra 'stop'
-
+const includes = leaveThisUncommented.includes('stop')
+console.log(leaveThisUncommented.includes('stop'))
 // 5.- Determina si leaveThisUncommented termina con la palabra 'here'
-
+const finishwith = leaveThisUncommented.trim().endsWith('here')
+console.log(finishwith)
 // 6.- Sustituye todas las "e" de leaveThisUncommented por "a"
-
+const changeAforE = leaveThisUncommented.toLowerCase().replaceAll('e','a')
+console.log(changeAforE)
 // 7.- Encuentra en qué posición de leaveThisUncommented se encuentra la palabra "Use"
-
+const whatPositon = leaveThisUncommented.search('Use')
+console.log(whatPositon)
 // 8.- Divide leaveThisUncommented por el punto
+const divide = leaveThisUncommented.split('.')
+console.log(divide)
 
 // 9.- Obtén el fragmento de leaveThisUncommented que va desde el índice 30 hasta el final
-
+const from = leaveThisUncommented.slice(30)
+console.log(from)
 // 10.- Obtén el fragmento de leaveThisUncommented que va desde el índice 30 hasta el 46
-
+const from2 = leaveThisUncommented.slice(30,46)
+console.log(from2)
--- 05.-numbers.js
+++ 05.-numbers.js
@@ -39,22 +39,33 @@
 
 // EJERCICIOS CON MÉTODOS Y MATH
 
-// const leaveThisUncommented = 156.34589;
+ const leaveThisUncommented = 156.34589;
 
 // 1. Cambia el tipo de leaveThisUncommented a string
-
+const changeToString= leaveThisUncommented.toString()
+console.log(changeToString)
 // 1. Redondea leaveThisUncommented a dos decimales
-
+const rounded = leaveThisUncommented.toFixed(2)
+console.log(rounded)
 // 3. Determina si leaveThisUncommented es un número entero
-
+const isEntero = Number.isInteger(leaveThisUncommented)
+console.log(isEntero)
 // 4. Determina si leaveThisUncommented es NaN
-
+const isNan = Number.isNaN(leaveThisUncommented)
+console.log(isNan)
 // 5. Castea leaveThisUncommented a número entero
-
+const castea = Number.parseInt(leaveThisUncommented);
+console.log(castea)
 // 6. Imprime el número PI
+const pi = Math.PI
+console.log(pi)
 
 // 7. Redondea leaveThisUncommented al número entero superior más cercano
-
+const roundedMax = Math.ceil(leaveThisUncommented)
+console.log(roundedMax)
 // 8. Redondea leaveThisUncommented al número entero inferior más cercano
-
+const roundedMin = Math.round(leaveThisUncommented)
+console.log(roundedMin);
 // 9. Genera un número entero aleatorio del 1 al 3
+const randomtotrhee = Math.floor(Math.random()*3 +1)
+console.log(randomtotrhee)
